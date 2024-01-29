85ef378ec626736b7d713b3304aa2a289daf1023
sohan11903 <104675668+sohan11903@users.noreply.github.com>
2024-01-29 19:22:37 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
+const SlotM = (props) =>{
+  // let x='😄';
+  // let y='😄';
+  // let z='🥶';
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+
+  if( (props.x===props.y) && (props.x===props.z)){
+    return(
+      <>
+        <div className='slot_inner'>
+          <h1>{" "}{x}{y}{z}{" "}</h1>
+          <h1>This is Matching.</h1>
+          <hr />
+        </div>
+      </>
+    );
+  }else{
+    return(
+      <>
+        <div className='slot_inner'>
+          <h1>{" "}{x}{y}{z}{" "}</h1>
+          <h1>This is not Matching.</h1>
+          <hr />
+        </div>
+      </>
+    );
+  }
+}
+const Footer = () =>
+{
+  return (
+
+  <>
+      <p className='footer'>if there is same then Matching otherwise not Matching using passing props</p>
+  </>
+  )
+ 
+}
 const  App = () =>{
   return <>
-    <h1 className='heading_style'> 🎰 Welcome to <span>Slot machine game</span> 🎰</h1>,
+    <h1 className='heading_style'>
+       🎰 Welcome to <span style={{fontWeight:"bold"}}>
+         Slot machine game</span> 🎰
+        </h1>,
+        <div className='container'>
+        <SlotM  x='😄' y='😄' z='😄'/>
+  <SlotM x='😄' y='😄' z='😄'/>
+  <SlotM x='😄' y='😄' z='😄'/>
+        </div>
+<Footer/>
+
+ 
   </>;
 }
 
